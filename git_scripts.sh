e363751f200f5849680000b2a9e60cc679fbbf51
V.Zaytsev <vialurt@gmail.com>
2019-07-18 10:56:52 +0300

    Update git_scripts.sh

--- git_scripts.sh
+++ git_scripts.sh
@@ -1,2 +1,5 @@
 # remove stale local branches
 git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git br -d
+
+# remove remote branches by specified argument
+git branch -r | grep $1 | sed "s/origin\///" | xargs git push origin --delete
